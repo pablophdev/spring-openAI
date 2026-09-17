fa7f3c7ea224cb4b5810d6e7debc4a4487bf5356
Pablo Philimon <pablo.philimon.zuniga@gmail.com>
2026-09-16 21:10:41 -0300

    feat: inicializando repositorio

---
+++ src/main/java/com/pabloph/spring_ai_introduction/SpringAiIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.pabloph.spring_ai_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pabloph/spring_ai_introduction/SpringAiIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabloph.spring_ai_introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
